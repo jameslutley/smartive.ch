06efafd3d5a99bc666cf4095a7db8a6b95244867
Moreno Feltscher <moreno@smartive.ch>
2017-08-03 14:46:44 +0200

    Fix Helmet

--- src/layouts/default-layout.js
+++ src/layouts/default-layout.js
@@ -8,14 +8,14 @@ const defaultTitle = '';
 
 export default ({ title, description, currentRoute, children }) => (
     <div>
-        <Helmet>
+        <Helmet
             <meta charset="utf-8" />
             <meta http-equiv="X-UA-Compatible" content="IE-edge,chrome=1" />
             <meta name="viewport" content="width=device-width,initial-scale=1" />
 
             <meta name="description" content={description ? description : defaultDescription} />
             <title>{title ? title : defaultTitle}</title>
-        </Helmet>
+        />
 
         <Header currentRoute={currentRoute} />
 
