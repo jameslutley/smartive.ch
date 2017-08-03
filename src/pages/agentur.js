5210ee81584abaf3d380d90f03acc0b408652e7e
Robert Vogt <robert@smartive.ch>
2017-08-03 17:45:57 +0200

    Use local image for agency page

--- src/pages/agentur.js
+++ src/pages/agentur.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Stage from '../components/stage';
 import TeamMember from '../components/team-member';
-import MediumTeaser from '../components/medium-teaser';
 
 import { team } from '../data/team';
+
+import teamStageSrc from '../data/team/thilo-peter-meeting.jpg';
 
 export default function Agency() {
   return (
@@ -11,9 +12,8 @@ export default function Agency() {
       <div className="stage--gradient stage--right-highlighted">
         <Stage
           image={{
-            src:
-              'https://prismic-io.s3.amazonaws.com/smartive/d12b7e81c6ea7d07260cf81ade530b35bee62f1e_team-babd8d3b.jpg',
-            alt: 'Drei smartive Mitarbeiter bei einer Besprechung',
+            src: teamStageSrc,
+            alt: 'Zwei smartive Mitarbeiter bei einer Besprechung',
           }}
           title={'<h1>Im <em>Herzen</em> von Zürich.</h1>'}
           description="Wir sind ein junges, dynamisches Team, bestehend aus acht Leuten. Unser breit gestreutes Wissen in sämtlichen Webbereichen unterstützt Sie dabei, sich und Ihr Unternehmen weiterzuentwickeln."
