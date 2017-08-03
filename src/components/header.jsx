96679ad98df967c79f000c4a8476b0e795ca294a
Robert Vogt <robert@smartive.ch>
2017-08-03 14:56:29 +0200

    Fix Navigation links

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,8 +5,7 @@ import PropTypes from 'prop-types';
 const items = [
   { href: '/projekte', label: 'Projekte' },
   { href: '/agentur', label: 'Agentur' },
-  { href: '/kontakt', label: 'Kontakt' },
-  { href: 'https://blog.smartive.ch', label: 'Blog', css: 'navigation-item--medium' },
+  { href: '/kontakt', label: 'Kontakt' }
 ];
 
 function getClassNames(currentRoute, item) {
@@ -15,35 +14,35 @@ function getClassNames(currentRoute, item) {
   return classes.filter(Boolean).join(' ');
 }
 
-const Header = ({ currentRoute }) => (
-  <header className="header">
+const Header = ({ currentRoute }) =>
+  (<header className="header">
     <div className="container">
       <div className="row middle-lg">
         <div className="header__col--logo col-xs-12 col-md-6">
-          <Link href="/">
-            <a className="logo">
-              <img src="/static/smartive.svg" alt="smartive" />
-            </a>
+          <Link to="/" className="logo">
+            <img src="/static/smartive.svg" alt="smartive" />
           </Link>
         </div>
 
         <div className="header__col--navigation col-xs-12 col-md-6">
           <nav className="navigation">
             <ul>
-              {items.map(item => (
-                <li key={item.label}>
-                  <Link to={item.href}>
-                    <a className={getClassNames(currentRoute, item)}>{item.label}</a>
+              {items.map(item =>
+                (<li key={item.label}>
+                  <Link to={item.href} className={getClassNames(currentRoute, item)}>
+                    {item.label}
                   </Link>
-                </li>
-              ))}
+                </li>),
+              )}
+              <li>
+                <Link className="navigation-item--medium" to="https://blog.smartive.ch" target="_blank">Blog</Link>
+              </li>
             </ul>
           </nav>
         </div>
       </div>
     </div>
-  </header>
-);
+  </header>);
 Header.propTypes = {
   currentRoute: PropTypes.string.isRequired,
 };
