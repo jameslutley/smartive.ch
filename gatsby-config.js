0bfd330f613207b7b78ae1ca43415fc4e4ca8de1
Moreno Feltscher <moreno@smartive.ch>
2017-08-03 15:44:02 +0200

    Remove unused files

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,27 +11,5 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 590,
-            },
-          },
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-autolink-headers',
-        ],
-      },
-    },
   ],
 };
