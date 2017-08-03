857e9e7c56b4e7a7552cd9d3049a9b7ed03bcfc9
Moreno Feltscher <moreno@smartive.ch>
2017-08-03 15:44:11 +0200

    Add proptypes to TeamMember

--- src/components/team-member.js
+++ src/components/team-member.js
@@ -1,11 +1,27 @@
-export default ({ image, job, name, description, education, links }) => (
-    <div className="member col-xs-12 col-md-6 col-lg-4">
-        <img className="member__portrait" src={image.src} alt={image.alt} />
-        <div className="member__text">
-            <h2><small>{job}</small> {name}</h2>
-            <h3>{education}</h3>
-            <div dangerouslySetInnerHTML={{ __html: description }} />
-            <div dangerouslySetInnerHTML={{ __html: links }} />
-        </div>
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TeamMember = ({ image, job, name, description, education, links }) => (
+  <div className="member col-xs-12 col-md-6 col-lg-4">
+    <img className="member__portrait" src={image.src} alt={image.alt} />
+    <div className="member__text">
+      <h2><small>{job}</small> {name}</h2>
+      <h3>{education}</h3>
+      <div dangerouslySetInnerHTML={{ __html: description }} />
+      <div dangerouslySetInnerHTML={{ __html: links }} />
     </div>
+  </div>
 );
+TeamMember.propTypes = {
+  image: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+  }).isRequired,
+  job: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  education: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  links: PropTypes.string.isRequired,
+};
+
+export default TeamMember;
