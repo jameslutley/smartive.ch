19e3c166c335ed79861ae96dd026977a42483ef8
Robert Vogt <robert@smartive.ch>
2017-08-03 15:57:20 +0200

    Re-add headroom

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import Headroom from 'react-headroom';
 
 import logo from './smartive.svg';
 
@@ -10,39 +11,41 @@ const items = [
 ];
 
 const Header = () =>
-  (<header className="header">
-    <div className="container">
-      <div className="row middle-lg">
-        <div className="header__col--logo col-xs-12 col-md-6">
-          <Link to="/" className="logo">
-            <img src={logo} alt="smartive" />
-          </Link>
-        </div>
+  (<Headroom>
+    <header className="header">
+      <div className="container">
+        <div className="row middle-lg">
+          <div className="header__col--logo col-xs-12 col-md-6">
+            <Link to="/" className="logo">
+              <img src={logo} alt="smartive" />
+            </Link>
+          </div>
 
-        <div className="header__col--navigation col-xs-12 col-md-6">
-          <nav className="navigation">
-            <ul>
-              {items.map(item =>
-                (<li key={item.label}>
-                  <Link to={item.href} activeClassName="is-active">
-                    {item.label}
+          <div className="header__col--navigation col-xs-12 col-md-6">
+            <nav className="navigation">
+              <ul>
+                {items.map(item =>
+                  (<li key={item.label}>
+                    <Link to={item.href} activeClassName="is-active">
+                      {item.label}
+                    </Link>
+                  </li>),
+                )}
+                <li>
+                  <Link
+                    className="navigation-item--medium"
+                    to="https://blog.smartive.ch"
+                    target="_blank"
+                  >
+                    Blog
                   </Link>
-                </li>),
-              )}
-              <li>
-                <Link
-                  className="navigation-item--medium"
-                  to="https://blog.smartive.ch"
-                  target="_blank"
-                >
-                  Blog
-                </Link>
-              </li>
-            </ul>
-          </nav>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
-  </header>);
+    </header>
+  </Headroom>);
 
 export default Header;
