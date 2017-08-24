11894f222415fd3093a59a17caeca98f156db991
Robert Vogt <robert@smartive.ch>
2017-08-24 17:14:25 +0200

    Rewrite all paths when building

---
+++ gatsby-node.js
@@ -0,0 +1,20 @@
+exports.onCreatePage = ({ page, boundActionCreators }) => {
+  const { createPage, deletePage } = boundActionCreators;
+
+  return new Promise((resolve, reject) => {
+    // Remove trailing slash
+    const oldPath = page.path;
+    // Removing '/' would result in a path that's
+    // an empty string which is invalid
+    page.path = page.path === '/' ? page.path : page.path.replace(/\/$/, '');
+    if (page.path !== oldPath) {
+      // Remove the old page
+      deletePage({ path: oldPath });
+
+      // Add the new page
+      createPage(page);
+    }
+
+    resolve();
+  });
+};
