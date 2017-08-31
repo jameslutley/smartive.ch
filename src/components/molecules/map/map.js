dd55dbdac9c6a138e3eeb038fab7a153995ca7f8
Moreno Feltscher <moreno@smartive.ch>
2017-08-31 12:34:38 +0200

    Add map marker image

--- src/components/molecules/map/map.js
+++ src/components/molecules/map/map.js
@@ -4,13 +4,14 @@ import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import withScriptjs from 'react-google-maps/lib/async/withScriptjs';
 
 import './map.scss';
+import mapMarkerImage from './map-marker.png';
 
 export const baseUrl = 'https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyD1q6iI1c4XGqrbKGBPZfO2bnRTRVaRg04';
 
 const AsyncGoogleMaps = withScriptjs(
   withGoogleMap(props =>
     (<GoogleMap ref={props.onMapLoad} defaultZoom={15} defaultCenter={{ lat: 47.3884069, lng: 8.5181335 }}>
-      <Marker icon="https://smartive.ch/assets/images/map-marker.png" position={{ lat: 47.3884069, lng: 8.5181335 }}>
+      <Marker icon={mapMarkerImage} position={{ lat: 47.3884069, lng: 8.5181335 }}>
         <InfoWindow>
           <div className="map__schema" itemScope itemType="https://schema.org/ContactPage">
             <p className="map-info">
